1a4398dd80c6ec87d7fd43801a4beb75a6973692
Flavio Santos <flavio.pasantos@outlook.com>
2023-01-30 19:25:56 -0300

    Initial commit

---
+++ spring_viacep/src/main/java/flavio/spring/viacep/spring_viacep/SpringViacepApplication.java
@@ -0,0 +1,13 @@
+package flavio.spring.viacep.spring_viacep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringViacepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringViacepApplication.class, args);
+	}
+
+}
---
+++ spring_viacep/src/test/java/flavio/spring/viacep/spring_viacep/SpringViacepApplicationTests.java
@@ -0,0 +1,13 @@
+package flavio.spring.viacep.spring_viacep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringViacepApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
